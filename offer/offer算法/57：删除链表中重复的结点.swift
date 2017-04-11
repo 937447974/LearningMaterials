caef8ae5b61086c539bed4dfa3675115ebdce17b
阳君 <937447974@qq.com>
2017-04-11 16:34:41 +0800

    offer/offer算法/57：删除链表中重复的结点.swift

--- offer/offer算法/57：删除链表中重复的结点.swift
+++ offer/offer算法/57：删除链表中重复的结点.swift
@@ -16,39 +16,43 @@ class LisNode: NSObject {
     var next: LisNode?
 }
 
-private func deleteFirstDuplication(node: LisNode?) -> LisNode? { // 去首重
-    guard node != nil && node!.next != nil && node!.value == node!.next!.value else {
-        return node
+private func deleteFirstDuplication(node: LisNode) -> LisNode? { //
+    var node = node
+    while node.next != nil && node.value == node.next!.value  {
+        var result = node.next
+        let value = node.value
+        while result != nil && result!.value == value  {
+            result = result!.next
+        }
+        guard result != nil else {
+            return nil
+        }
+        node = result
     }
-    let duplication = node!.value
-    var result = node!.next!.next
-    while result != nil && result?.value == duplication {
-        result = result!.next
-    }
-    return deleteFirstDuplication(node: result)
+    return node
 }
 
 func deleteDuplication(node: LisNode) -> LisNode? {
+    guard node.next != nil else {
+        return node
+    }
+    // 去首重
     guard let result = deleteFirstDuplication(node: node) else {
         return nil
     }
-    var tempNode = result
+    node = result
+    // 去中间重
+    var tempNode = node
     var currentNode = tempNode.next
-    var currentNextNode = currentNode?.next
-    while currentNode != nil && currentNextNode != nil {
-        if currentNode!.value == currentNextNode!.value {
-            let duplication = currentNode!.value
-            currentNode = currentNextNode?.next
-            tempNode.next = currentNode
-            while currentNode != nil && currentNode!.value == duplication {
-                currentNode = currentNode?.next
-                tempNode.next = currentNode
-            }
+    while currentNode != nil {
+        let resultNode = deleteFirstDuplication(node: currentNode)
+        tempNode.next = resultNode
+        tempNode = resultNode
+        if currentNode!.value == resultNode.value {
+            currentNode = resultNode.next
         } else {
-            tempNode = currentNode!
+            currentNode = resultNode
         }
-        currentNode = tempNode.next
-        currentNextNode = currentNode?.next
     }
-    return result
+    return node
 }
